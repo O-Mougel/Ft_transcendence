3f02ced84c82534c81e26f5edb934b01fcfb6d63
vdomasch <vincent.domaschio@gmail.com>
2025-10-29 16:47:47 +0100

    pong backend

---
+++ srcs/back/pong/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const cors = require('cors');
+
+
+
+app.use(cors({
+  origin: "*"
+}));
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world -- </h1>');
+});
+
+let rooms = [];
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on 3000');
+});
