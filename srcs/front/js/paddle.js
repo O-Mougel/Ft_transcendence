e83370751e6f365b010fee34cbc523c6f0896862
vdomasch <vincent.domaschio@gmail.com>
2025-11-05 16:47:34 +0100

    pong: good reset; ball random spawn direction; adjust direction rebound; change ball color + ball speed option added

--- srcs/front/js/paddle.js
+++ srcs/front/js/paddle.js
@@ -15,29 +15,8 @@ export default class Paddle {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 
-    moveUp() {
-        this.y -= this.speed;
-    }
-
-    moveDown() {
-        this.y += this.speed;
-    }
-
-    increaseScore() {
-        this.score += 1;
-    }
-
-    resetScore() {
+    reset() {
+        this.y = 250 - this.height / 2;
         this.score = 0;
-    }
-
-    setSpeed(newSpeed) {
-        this.speed = newSpeed;
-    }
-
-    printScore() {
-        ctx.fillStyle = 'white';
-        ctx.font = '20px Arial';
-        ctx.fillText(this.score, this.x + this.width / 2, 20);
     }
 }
