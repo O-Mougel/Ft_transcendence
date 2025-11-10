29d052910176e4a751298e12aead2f11b4bcaee4
Leo Chapard <lchapard@z4r6p4.42lyon.fr>
2025-11-10 16:33:00 +0100

    added more responsivity

--- srcs/requirements/front/tailwindcss/src/views/selectModes.js
+++ srcs/requirements/front/tailwindcss/src/views/selectModes.js
@@ -9,14 +9,15 @@ export default class extends ViewTemplate {
 
 	async getHTML() {
 		return `
-				<div class="h-screen w-full flex">
-					<div class="flex-1 box-border border-10 bg-[url(/img/vs1.jpg)] bg-cover blur-[10px] transition duration-400 ease-in-out hover:blur-[0px] flex items-center justify-center ">
-						<!-- <div class="w-1/3 h-1/4 bg-[#4d408f] flex items-center justify-center border border-black">Versus AI</div>  -->
-					</div>
-					<div class="flex-1 box-border border-10 bg-[url(/img/vsPlayer.png)] bg-cover blur-[10px] transition duration-400 ease-in-out hover:blur-[0px] flex items-center justify-center">
-			
-					</div>
+			<div class="h-screen w-full flex">
+				<div class="flex-1 rounded-xl box-border border-10 bg-[url(/img/vs1.jpg)] bg-cover grayscale-85 hover:grayscale-0 blur-[5px] hover:blur-[0px] transition duration-400 ease-in-out   flex items-center justify-center ">
+					<!-- <div class="w-1/3 h-1/4 bg-[#4d408f] flex items-center justify-center border border-black">Versus AI</div>  -->
 				</div>
-				<script type="module" src="/js/index.js"></script>`
+				<div class="flex-1 rounded-xl box-border border-10 bg-[url(/img/vsPlayer.png)] bg-cover grayscale-85 hover:grayscale-0 blur-[5px] hover:blur-[0px] transition duration-400 ease-in-out  flex items-center justify-center">
+		
+				</div>
+			</div>
+			<a href="/" data-link>Back to login!</a>
+			<script type="module" src="/js/index.js"></script>`
 	}
 }
