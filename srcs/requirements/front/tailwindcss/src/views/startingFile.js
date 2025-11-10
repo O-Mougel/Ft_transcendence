fdab49790a452ff24e3a11eda7fa1092f14e05bd
Leo Chapard <leochap@free.fr>
2025-11-10 16:33:00 +0100

    added more responsivity

--- srcs/requirements/front/tailwindcss/src/views/startingFile.js
+++ srcs/requirements/front/tailwindcss/src/views/startingFile.js
@@ -10,17 +10,17 @@ export default class extends ViewTemplate {
 	async getHTML() {
 		return `
 		<body class="">
-			<div class="h-screen bg-[url(/img/loginUnblured.jpg)] backdrop-blur-[10px] pt-15 text-center ml-auto mr-auto 	">
+			<div class="min-h-screen bg-[url(/img/loginUnblured.jpg)] bg-cover backdrop-blur-[10px] pt-15 text-center ml-auto mr-auto">
 				<img class ="mx-auto mb-[75px]" id="logo" src="./img/sillyDog.gif">
 				<form>
 					<div class="content-center">
-						<input tabindex="1" class="mx-auto text-white w-[400px] h-[35px] text-[21px] rounded-s border border-2 border-black pt-[8px] pb-[5px] pr-[20px] pl-[20px] mb-[25px]" name="playerUsername" value="" type="text" autofocus autocomplete="off" placeholder="Enter your login" aria-invalid>
+						<input tabindex="1" class="mx-auto text-white text-ellipsis w-1/2 md:w-1/4 h-[35px] text-sm md:text-base rounded-s border border-2 border-black pt-[8px] pb-[5px] pr-[20px] pl-[20px] mb-[25px]" name="playerUsername" value="" type="text" autofocus autocomplete="off" placeholder="Enter your login" aria-invalid>
 					</div>
 					<div class="content-center">
-						<input tabindex="2" class="mx-auto text-white w-[400px] h-[35px] text-[21px] rounded-sm border border-2 border-black pt-[8px] pb-[5px] pr-[20px] pl-[20px] mb-[35px]" name="playerPassword" value="" type="text" autocomplete="off" placeholder="Enter your password" aria-invalid>
+						<input tabindex="2" class="mx-auto text-white text-ellipsis w-1/2 md:w-1/4 h-[35px] text-sm md:text-base rounded-sm border border-2 border-black pt-[8px] pb-[5px] pr-[20px] pl-[20px] mb-[35px]" name="playerPassword" value="" type="text" autocomplete="off" placeholder="Enter your password" aria-invalid>
 					</div>
 					<div class="">
-						<input tabindex="3" class="uppercase text-4xl bg-blue-500 w-100 font-bold pt-[8px] pb-[8px] rounded-lg text-white" name="login" type="submit" value="Sign In">
+						<input tabindex="3" class="uppercase text-sm md:text-base xl:text-2xl text-ellipsis bg-blue-500 w-1/2 md:w-1/4 font-bold pt-[8px] pb-[8px] rounded-lg text-white" name="login" type="submit" value="Sign In">
 					</div>
 					<a href="/modes" data-link>Click for modes !</a>
 				</form>
