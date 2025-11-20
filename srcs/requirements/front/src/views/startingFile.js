328a04a8ef58791a9f7c41aa77fe40a371add1e2
Benjamin Humeau <bhumeau@z4r2p4.42lyon.fr>
2025-11-20 15:48:27 +0100

    added font 'space' as default font-family

--- srcs/requirements/front/src/views/startingFile.js
+++ srcs/requirements/front/src/views/startingFile.js
@@ -19,7 +19,7 @@ export default class extends ViewTemplate {
 						<input tabindex="2" class="mx-auto text-white hover:text-[#98c6f8] text-ellipsis focus:outline-none focus:border-[#98c6f8] hover:border-[#98c6f8] w-1/2 md:w-1/4 h-[35px] text-sm md:text-base rounded-sm border border-[#c2dbf6] pt-2 pb-[5px] pr-5 pl-5 mb-[35px]" name="playerPassword" value="" type="text" autocomplete="off" placeholder="Enter your password" aria-invalid>
 					</div>
 					<div class="">
-						<input tabindex="3" class="uppercase text-sm md:text-base xl:text-2xl text-white focus:outline-none focus:border-[#98c6f8] hover:text-[#98c6f8] text-ellipsis w-1/2 md:w-1/4 font-bold border hover:border-[#98c6f8] border-white pt-2 pb-2 rounded-lg " name="login" type="submit" value="Sign In">
+						<input tabindex="3" class="uppercase text-sm md:text-base xl:text-2xl text-white focus:outline-none focus:border-[#98c6f8] hover:text-[#98c6f8] text-ellipsis w-1/2 md:w-1/4 border hover:border-[#98c6f8] border-white pt-2 pb-2 rounded-lg " name="login" type="submit" value="Sign In">
 					</div>
 					<a href="/modes" data-link>Click for modes !</a>
 					<a href="/nav" data-link>Click for nav !</a>
