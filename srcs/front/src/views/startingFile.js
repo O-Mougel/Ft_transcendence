3e9e5da13ac0b1f09df0c2068483148a5db13880
Leo Chapard <lchapard@z4r3p2.42lyon.fr>
2025-11-25 15:47:38 +0100

    fixing navbar and profile

--- srcs/front/src/views/startingFile.js
+++ srcs/front/src/views/startingFile.js
@@ -9,7 +9,19 @@ export default class extends ViewTemplate {
 
 	async getHTML() {
 		return `
+			<div id="profilePanel" class="flex absolute right-0 top-0 h-full w-[15%] bg-[url(/img/stars.gif)] z-50 shadow-md border border-[#98c6f8] border border-[#98c6f8]">
+					<div class="flex flex-col text-center w-full">
+						<div class="grid h-[30%] place-items-center">
+  							<div class="bg-[url(/img/sillyDog.gif)] bg-cover w-[50%] h-[50%] p-4 rounded-[50%] shadow"></div>
+						</div>
+						<h1 class="text-white text-bold h-1/5 text-4xl">Goofy ahh player</h1>
+						<p class="text-white">Plaisantin</p>
+						<p class="text-white">Wow what a great person</p>
+						<button class="text-white">Click for stats !</button>
+					</div>
+			</div>
 			<div class="text-center ml-auto mr-auto pt-[5%]">
+			
 				<img class ="mx-auto mb-[75px]" id="logo" src="./img/sillyDog.gif">
 				<form>
 					<div class="content-center">
@@ -21,8 +33,6 @@ export default class extends ViewTemplate {
 					<div class="">
 						<input tabindex="3" class="uppercase text-sm md:text-base xl:text-2xl text-white focus:outline-none focus:border-[#98c6f8] hover:text-[#98c6f8] text-ellipsis w-1/2 md:w-1/4 border hover:border-[#98c6f8] border-white pt-2 pb-2 rounded-lg " name="login" type="submit" value="Sign In">
 					</div>
-					<a href="/modes" data-link class="text-red-200">Click for modes !</a>
-					<a href="/nav" data-link class="text-red-200">Click for nav !</a>
 				</form>
 			</div>`
 	}
