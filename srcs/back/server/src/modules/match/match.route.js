cfbad17c07ef4071d51fe64d81ff0ade8be98db2
omougel <omougel@student.42lyon.fr>
2025-12-02 16:28:46 +0100

    adding matchs

--- srcs/back/server/src/modules/match/match.route.js
+++ srcs/back/server/src/modules/match/match.route.js
@@ -1,11 +1,23 @@
 
+// user.route.ts
 
+import { $ref } from "./match.schema.js";
+import { createMatchHandler } from "./user.controller.js";
+
+async function matchRoutes(fastify) {
 	fastify.post(
 		'/match',
 		{
 			preHandler: [fastify.authenticate],
 			schema: {
-				body: $ref("matchSchema")
-			}
-		}
+				body: $ref("createMatchSchema"),
+				response: {
+					201: $ref("createMatchResponseSchema")
+				},
+			},
+		},
+		createMatchHandler,
 	)
+}
+
+export default matchRoutes;
