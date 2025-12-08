ddd33daa401debdc3b9ae456ed50cd5f6f211a55
vdomasch <vincent.domaschio@gmail.com>
2025-12-08 17:27:10 +0100

    add path to pong and 2vs2 pong; patch stuck ball ?

--- srcs/front/src/views/pong.js
+++ srcs/front/src/views/pong.js
@@ -9,36 +9,53 @@ export default class extends ViewTemplate {
 
 	async getHTML() {
 		return `
-	<div class="h-full flex w-full justify-center text-white">
-		<div class="pt-5 flex flex-col gap-4 items-center">
-			<canvas id="canvas" class="border-4 rounded-[2%] border-[#98c6f8] w-[64dvw] aspect-[16/10]"></canvas>
-			<div id="GameOver" class="hidden absolute border-4 rounded-[10%] border-[#98c6f8] justify-center items-center p-4 mt-4">
-				<div class="flex items-center">
-					<span>Game Over</span>
-				</div>
-				<div class="flex justify-center items-center">					
-					<span id="GameOverScore">0 - 0</span>
-				</div>
-			</div>
-			<button id="startButton" class="px-6 py-3 bg-[#98c6f8] text-white font-bold rounded-lg hover:bg-[#7aaedc]">Start Game</button>
-			
-			<!-- Score row under the canvas -->
-			<div id="Scores" class="flex justify-center items-center text-white font-bold hidden">
-				<div class="flex items-center">
-					<span>Left</span>
-					<h3 class="p-4" id="ScoreLeft" value="0">0</h3>
-				</div>
-				<span></span>
-				<div class="flex items-center">
-					<h3 class="p-4" id="ScoreRight" value="0">0</h3>
-					<span>Right</span>
+			<div id="profilePanel" class="flex absolute right-0 top-0 h-full w-[20%] bg-[url(/img/stars.gif)] z-50 shadow-[0_0_20px_rgba(158,202,237,0.9)] border border-[#98c6f8]">
+				<div class="flex flex-col text-center w-full h-full">
+					<div class="grid h-[30%] place-items-center">
+  						<div class="bg-[url(/img/sillyDog.gif)] bg-cover w-[50%] h-[50%] p-4 rounded-[50%] shadow"></div>
+					</div>
+					<h1 class="text-white text-bold mx-4 mb-15 text-2xl ">[player username]</h1>
+					<a href="/profileOverview" class="mx-4 text-2xl mb-15 border p-4 text-[#798490]" name="profileLink" data-link>Update profile</a>
+					<div class="self-start">
+						<input class="sr-only peer" id="friendCheck" type="checkbox"/>
+						<label for="friendCheck" class="text-2xl cursor-pointer px-3 py-2 select-none hover:text-[#98c6f8]">► Online friends</label>
+						<ul class="select-none peer-checked:block hidden mt-3 self-start text-left" id="friendlist">
+						<li><a class="text-xl pl-2">• Goug ?</a></li>
+						<li><a class="text-xl pl-2">• Gougou ?</a></li>
+						<li><a class="text-xl pl-2">• Gougougaga ?</a></li>
+						</ul>
+					</div>
 				</div>
 			</div>
-		</div>
+			<div class="h-full flex w-full justify-center text-white">
+				<div class="pt-5 flex flex-col gap-4 items-center">
+					<canvas id="canvas" class="border-4 rounded-[2%] border-[#98c6f8] w-[64dvw] aspect-[16/10]"></canvas>
+					<div id="GameOver" class="hidden absolute border-4 rounded-[10%] border-[#98c6f8] justify-center items-center p-4 mt-4">
+						<div class="flex items-center">
+							<span>Game Over</span>
+						</div>
+						<div class="flex justify-center items-center">					
+							<span id="GameOverScore">0 - 0</span>
+						</div>
+					</div>
+					<button id="startButton" class="px-6 py-3 bg-[#98c6f8] text-white font-bold rounded-lg hover:bg-[#7aaedc]">Start Game</button>
 
-	</div>
-	<script src="/game/main.js" type="module"></script>
-			`
+					<!-- Score row under the canvas -->
+					<div id="Scores" class="flex justify-center items-center text-white font-bold hidden">
+						<div class="flex items-center">
+							<span>Left</span>
+							<h3 class="p-4" id="ScoreLeft" value="0">0</h3>
+						</div>
+						<span></span>
+						<div class="flex items-center">
+							<h3 class="p-4" id="ScoreRight" value="0">0</h3>
+							<span>Right</span>
+						</div>
+					</div>
+				</div>
+
+			</div>
+			<script src="/game/main.js" type="module"></script>`
 	}
 
 	async init() {
