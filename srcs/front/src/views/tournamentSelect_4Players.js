ed2658ae69d95175a1234319de2010d451d99253
Benjamin Humeau <bhumeau03@gmail.com>
2025-12-10 15:20:11 +0100

    added pages 4-8-16 players, values not stored yet

---
+++ srcs/front/src/views/tournamentSelect_4Players.js
@@ -0,0 +1,25 @@
+import ViewTemplate from "./ViewTemplate.js";
+
+export default class extends ViewTemplate {
+	constructor()
+	{
+		super();
+		this.setTitle("Select Tournament Size");
+	}
+
+	async getHTML() {
+		return `
+
+		<div class="h-full w-full">
+			<form id="tournamentForm4Players" class="mt-[5vw]">
+					<input id="player1" tabindex="1" class="w-[50vw] mt-[1vw] pl-5 mx-auto hover:text-[#98c6f8]  focus:outline-none focus:border-[#98c6f8] hover:border-[#98c6f8]-[35px] rounded-sm border border-[#c2dbf6]" type="text" autofocus autocomplete="off" placeholder="Player 1">
+					<input id="player2" tabindex="2" class="w-[50vw] mt-[1vw] pl-5 mx-auto hover:text-[#98c6f8]  focus:outline-none focus:border-[#98c6f8] hover:border-[#98c6f8]-[35px] rounded-sm border border-[#c2dbf6]" type="text" autocomplete="off" placeholder="Player 2">
+					<input id="player3" tabindex="3" class="w-[50vw] mt-[1vw] pl-5 mx-auto hover:text-[#98c6f8]  focus:outline-none focus:border-[#98c6f8] hover:border-[#98c6f8]-[35px] rounded-sm border border-[#c2dbf6]" type="text" autocomplete="off" placeholder="Player 3">
+					<input id="player4" tabindex="4" class="w-[50vw] mt-[1vw] pl-5 mx-auto hover:text-[#98c6f8]  focus:outline-none focus:border-[#98c6f8] hover:border-[#98c6f8]-[35px] rounded-sm border border-[#c2dbf6]" type="text" autocomplete="off" placeholder="Player 4">
+				<div class="">
+					<input tabindex="5" class="w-[20vw] mt-[1vw] h-[4vw] focus:outline-none focus:border-[#98c6f8] hover:text-[#98c6f8] border hover:border-[#98c6f8] border-white rounded-lg " name="start4Players" type="submit" value="Start" onclick="get4PlayerName(event)">
+				</div>
+			</form>
+		</div>`
+	}
+}
