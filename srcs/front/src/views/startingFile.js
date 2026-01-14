4df2716e3204e5eab24c4c05707244d58ab47a83
Benjamin Humeau <bhumeau03@gmail.com>
2026-01-14 17:23:19 +0100

    love-star to white-star

--- srcs/front/src/views/startingFile.js
+++ srcs/front/src/views/startingFile.js
@@ -44,7 +44,7 @@ export default class extends ViewTemplate {
 			<div class="pt-[5vw] h-full w-full">
 				<h1 class="text-[5vw] sm:text-[5vw] 2xl:text-[4vw] my-6 animate-pulse">FT_Transcendence</h1>
 				<div class=" w-full h-[25%] my-15 content-center m-auto">
-					<img src="/img/assets/love-star.png" class="block w-[50%] max-w-5xl m-auto mb-15 aspect-square" >
+					<img src="/img/assets/death-star.png" class="block w-[50%] max-w-5xl m-auto mb-15 aspect-square" >
 					<a id="mainPageLoginButton" href="/logUser" class="hidden uppercase text-sm md:text-base xl:text-2xl focus:outline-none focus:border-[#98c6f8] hover:text-[#98c6f8] text-ellipsis w-1/2 md:w-1/4 border hover:border-[#98c6f8] border-white py-2 px-4 rounded-lg shadow-[0_0_20px_rgba(158,202,237,0.9)]" name="logPageLink" data-link> 🚀 Sign in </a>
 				</div>
 			</div>`
