ebd2c9620aa96a7b191693c3291a4ba80d52eee6
Leo Chapard <lchapard@z4r6p4.42lyon.fr>
2026-01-21 18:15:38 +0100

    new zod error messages

--- srcs/back/server/src/modules/match/match.service.js
+++ srcs/back/server/src/modules/match/match.service.js
@@ -1,7 +1,6 @@
 // match.service.js
 
 import { db } from "../../utils/prisma.js";
-import { findUserByName } from "../user/user.service.js"
 
 export async function createMatch(input) //player1Id, player2Id(only in ranked), player1Score, player2Score, winnerId (-1 if aborted), longestStreak, duration, finish, type, other value if they are existent 
 {
@@ -22,13 +21,19 @@ export async function showstats(id) {
 			finish: true,
 		},
 	})
+
+	if(matchsnb == 0) // if never played before
+	{
+		return { matchsnb:0, winrate:0, biggest_streak:0, longestMatch:0 } // we need numbers for zod schema
+	}
+	
 	const win = await db.match.count({
 		where: {
 			winnerId: id,
 			finish: true,
 		},
 	})
-	const calc = win / matchsnb * 100;
+	const calc = win / matchsnb * 100; //divide by zero ?
 	const winrate = calc.toFixed(2);
 
 	const result = await db.match.aggregate({
@@ -45,10 +50,24 @@ export async function showstats(id) {
 		}
 	});
 
+	console.log("\n\n\n------");
+	console.log(matchsnb);
+	console.log("\n\n\n------");
+	console.log("\n\n\n------");
+	console.log(result);
+	console.log("-------\n\n\n");
+	if (result)
+	{
 	const biggest_streak = result.max.longestStreak ?? 0;
 	const longestMatch = result.max.duration ?? 0;
-
+	}
+	else
+	{
+		biggest_streak = 0;
+		longestMatch = 0;
+	}
 	return { matchsnb, winrate, biggest_streak, longestMatch } 
+
 }
 
 export async function getMatchs(id) {
