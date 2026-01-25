cc93f882dda242807a0987cde3d399aa1ae128f9
Benjamin Humeau <bhumeau03@gmail.com>
2026-01-25 14:42:22 +0100

    about page

--- srcs/front/src/ts/gameTS/controls.ts
+++ srcs/front/src/ts/gameTS/controls.ts
@@ -63,7 +63,8 @@ function updateDirections(): void {
   else if (s && !w) leftPaddle.direction = "down";
   else leftPaddle.direction = "none";
 
-  if (state.left !== leftPaddle.direction || state.right !== rightPaddle.direction) updateGameState();
+  //if (state.left !== leftPaddle.direction || state.right !== rightPaddle.direction) updateGameState();
+  updateGameState();
 
 
   if (CONTEXT.gameMode !== 2) return;
@@ -84,5 +85,6 @@ function updateDirections(): void {
   else if (keysPressed.has("3") && !keysPressed.has("6")) rightPaddle.direction = "down";
   else rightPaddle.direction = "none";
 
-  if (state2.left !== leftPaddle2.direction || state2.right !== rightPaddle2.direction) updateGameState();
+  //if (state2.left !== leftPaddle2.direction || state2.right !== rightPaddle2.direction) updateGameState();
+  updateGameState();
 }
