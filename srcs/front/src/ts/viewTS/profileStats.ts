82a47258a01e8744b3f16fdd1e664e9945936a8b
Leo Chapard <lchapard@z4r4p6.42lyon.fr>
2026-01-29 16:29:22 +0100

    bigger menus and buttons

--- srcs/front/src/ts/viewTS/profileStats.ts
+++ srcs/front/src/ts/viewTS/profileStats.ts
@@ -11,42 +11,42 @@ export default class ProfileStatsView extends ViewTemplate {
 			<div id="profilePanel" class="hidden absolute animate-slide-in-left right-0 top-0 h-full min-w-80 w-[20%] bg-[url(/img/assets/stars.gif)] z-50 shadow-[0_0_20px_rgba(158,202,237,0.9)] border border-[#98c6f8] overflow-auto">
 				<div class="flex flex-col text-center w-full h-full">
 					<div class="grid h-[30%] place-items-center">
-						<div id="sidePannelPfp" class="bg-[url('/img/userPfp/default.png')] bg-cover p-4 rounded-[50%] opacity-0 shadow object-cover w-[170px] h-[170px]"></div>
+						<div id="sidePannelPfp" class="bg-[url('/img/userPfp/default.png')] bg-cover p-4 rounded-[50%] opacity-0 shadow object-cover sm:w-[170px] sm:h-[170px] md:w-[18vh] md:h-[18vh]"></div>
 					</div>
-					<h1 id="playerGrabbedUsername" class="text-black text-bold underline mx-4 mb-10 text-2xl "></h1>
-					<a href="/profileStats" class="mx-4 text-2xl mb-5 border p-4 text-[#798490] hover:text-[#98c6f8]" name="profileLink" data-link>View profile</a>
-					<a href="/customizeProfile" class="mx-4 text-2xl mb-5 border p-4 text-[#798490] hover:text-[#98c6f8]" name="profileLink" data-link>Update profile</a>
-					<a id="logoutButton" class="mx-4 text-2xl mb-8 border p-4 cursor-pointer hover:text-[#dee9f4] hover:bg-[#882639] text-[#882639]" onclick=logoutUser() name="logoutButtonName">➜] Log out</a>
-					<div class="self-start px-4">
+					<h1 id="playerGrabbedUsername" class="text-black text-bold underline mx-4 mb-10 text-2xl lg:text-5xl "></h1>
+					<a href="/profileStats" class="mx-4 text-2xl lg:text-4xl mb-5 border p-4 text-[#798490] hover:text-[#98c6f8]" name="profileLink" data-link>View profile</a>
+					<a href="/customizeProfile" class="mx-4 text-2xl lg:text-4xl mb-5 border p-4 text-[#798490] hover:text-[#98c6f8]" name="profileLink" data-link>Update profile</a>
+					<a id="logoutButton" class="mx-4 text-2xl lg:text-4xl mb-8 border p-4 cursor-pointer hover:text-[#dee9f4] hover:bg-[#882639] text-[#882639]" onclick=logoutUser() name="logoutButtonName">➜] Log out</a>
+					<div class="self-start px-4 w-full">
 						<input class="sr-only peer" id="friendCheck" type="checkbox"/>
-						<label for="friendCheck" class="text-2xl cursor-pointer py-2 mb-10 select-none hover:text-[#98c6f8]">➤ Friend list</label>
+						<label for="friendCheck" class="text-2xl lg:text-3xl cursor-pointer py-2 mb-10 select-none hover:text-[#98c6f8]">➤ Friend list</label>
 						<ul class="select-none peer-checked:block hidden mt-3 ml-2 self-start text-left animate-slide-fade-up" id="friendlist"></ul>
 					</div>
-					<div id="pendingRequestBlock" class="w-full mt-8 px-4 hidden">
+					<div id="pendingRequestBlock" class="w-full mt-8 lg:mt-15 px-4 hidden">
 							<input class="sr-only peer" id="requestCheck" type="checkbox" />
-							<label for="requestCheck" id="requestCheckLabel" class="text-xl cursor-pointer mt-10 px-3 py-2 select-none hover:text-[#98c6f8] ">
+							<label for="requestCheck" id="requestCheckLabel" class="text-xl lg:text-3xl cursor-pointer mt-10 px-3 py-2 select-none hover:text-[#98c6f8] ">
 								► Requests
 							</label>
 							<ul id="requestList" class="select-none peer-checked:block hidden mt-3 self-start text-left animate-slide-fade-up"></ul>
 					</div>
 					<div class="w-full mt-3 px-4 flex">
-						<div class="w-[85%] max-w-60">
-							<div class="flex items-center py-2 w-full mt-5">
-								<input id="friendSearchInput" type="text" placeholder="Add friend" maxlength="13" class="h-8 px-2 rounded-l-md border border-white bg-transparent focus:outline-none text-sm" />
-								<button id="friendSearchButton" class=" h-8 bg-[#98c6f8] text-black rounded-r-md text-sm" onclick=sendNewFriendRequest()>🔍</button>
-							</div>
-							<p id="friendSearchResults" class="mt-4 text-base text-ellipsis"></p>
+						<div class="w-full ">
+							<form class="flex items-center py-2 h-full w-full mt-5">
+								<input id="friendSearchInput" type="text" placeholder="Add friend" maxlength="13" class="h-[70%] w-[80%] lg:text-3xl px-2 rounded-l-md border border-white bg-transparent focus:outline-none text-sm" />
+								<button id="friendSearchButton" class=" h-[70%] w-[10%] bg-[#98c6f8] text-black rounded-r-md text-sm lg:text-3xl type="submit" onclick="sendNewFriendRequest(event)" >🔍</button>
+							</form>
+							<p id="friendSearchResults" class="mt-4 text-2xl text-red-500 text-ellipsis"></p>
 						</div>
 					</div>
 				</div>
 			</div>
-
+			
 			<div class="pt-[3%] mt-[3%] relative flex flex-col gap-y-4 items-center mx-[3%] px-4 rounded-xl outline-none border border-blue-300 bg-[url(/img/assets/stars.gif)] bg-cover bg-center shadow-[0_0_20px_rgba(158,202,237,0.9)]">
 			<h1 class="m-0">Statistics :</h1>
 			<div class="flex flex-col sm:flex-row w-full justify-around items-center gap-4">
 				<div class="flex flex-col items-center">
 						<h2 id="playerUsernameProfile" class="hidden text-center pr-2 underline"></h2>
-						<img id="userPfpProfile" src="./img/userPfp/default.png" alt="userPfpImg" class="hidden pl-2 pt-4 w-20 h-20 sm:w-[120px] sm:h-[120px] object-cover shrink-0 select-none" draggable="false" />
+						<img id="userPfpProfile" src="./img/userPfp/default.png" alt="userPfpImg" class="hidden pl-2 pt-4 w-20 h-20 sm:w-[120px] sm:h-[120px] md:w-[10vw] md:h-[10vw] object-cover shrink-0 select-none" draggable="false" />
 					</div>
 					<a class="uppercase px-5 py-1 z-20 focus:outline-none focus:border-[#98c6f8] hover:text-[#98c6f8] text-ellipsis border hover:border-[#98c6f8] border-white rounded-lg" name="gotoMatchHistory" href="/UserMatchHistory" data-link>View match history</a>
 				</div>
