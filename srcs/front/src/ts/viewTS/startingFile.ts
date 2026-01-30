396481944087776c0efbecb3fb715bc3a329b354
Leo Chapard <lchapard@z4r4p3.42lyon.fr>
2026-01-30 19:25:36 +0100

    added secret ;)

--- srcs/front/src/ts/viewTS/startingFile.ts
+++ srcs/front/src/ts/viewTS/startingFile.ts
@@ -42,7 +42,7 @@ export default class StartingFileView extends ViewTemplate {
 			</div>
 
 			<div class="pt-[5vw] h-full w-full">
-				<h1 id="bigMainTitle" class="text-center mx-auto text-[5vw] sm:text-[5vw] 2xl:text-[4vw] w-[80%] my-6 animate-wiggle">FT_Transcendence</h1>
+				<h1 id="bigMainTitle" class="text-center mx-auto text-[5vw] sm:text-[5vw] 2xl:text-[4vw] w-[80%] my-6 animate-wiggle" onclick="ft_bh()">FT_Transcendence</h1>
 				<div class=" w-full h-[25%] my-15 content-center m-auto">
 					<img id="deathStarImg" src="/img/assets/death-star.png" class="block w-[40%] max-w-3xl m-auto mb-15 aspect-square select-none" onclick=spinMeAround() draggable="false" />
 					<div id="loginButtonContainer" class="animate-float">
