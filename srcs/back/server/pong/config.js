94def426381d3b4bdd6a721f51337489b69c4b66
vdomasch <vincent.domaschio@gmail.com>
2026-01-31 13:51:51 +0100

    ball speed + score

--- srcs/back/server/pong/config.js
+++ srcs/back/server/pong/config.js
@@ -5,13 +5,13 @@ export const PADDLE_WIDTH = 10;
 export const PADDLE_HEIGHT = 80;
 
 export const BALL_RADIUS = 10;
-export const BASE_BALL_SPEED = 2.0;
+export const BASE_BALL_SPEED = 4.0;
 export const MAX_BALL_SPEED = 15.0;
 export const STEP = 0.5;
 export const BASE_PADDLE_SPEED = 6.0;
 export const TICK_RATE = 1000 / 60; // 60 FPS
 export const AI_REACTION_TIME = 1000; // in ms
-export const WIN_SCORE = 2;
+export const WIN_SCORE = 5;
 
 export const DISCONNECT_GRACE_PERIOD = 30000; // 30 seconds
 export const TOURNAMENT_TIMEOUT = 4 * 60 * 60 * 1000; // 4 hours
