bf6f25b60b5061835e1f769d9abb8bda2cb2246f
Ophelia Mougel <omougel@z4r5p2.42lyon.fr>
2026-01-31 15:41:46 +0100

    added heartbeat

--- srcs/front/src/ts/gameTS/socket.ts
+++ srcs/front/src/ts/gameTS/socket.ts
@@ -39,11 +39,12 @@ export function setupSocket(): SocketType | null {
 		try {
 			const result = reasonSchema.validateSync({ reason });
 			console.log("WebSocket disconnected:", result);
-			backToDefaultPage();
+			console.log("TEST");
+			if (sessionStorage.getItem("currentTournamentId"))
+				sessionStorage.delete("currentTournamentId");
 		}
 		catch (err) {
 			console.error("Error parsing disconnect reason:", err);
-			backToDefaultPage();
 		}
 	});
 
